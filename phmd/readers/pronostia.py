c7170979df958d39c751530a1592de5f1afad6f4
levtelyatnikov <tlscabinet@gmail.com>
2025-10-30 09:13:23 -0700

    Updated RUL of PRONOSTIA as one file records 1/10 sec, hence the RUL is constant for each file

--- phmd/readers/pronostia.py
+++ phmd/readers/pronostia.py
@@ -98,7 +98,7 @@ def read_files(file_path, dirs, files, z=None, RULS=None, filters: dict = None):
             X = X.reset_index(drop=True)
 
             # compute RUL
-            X["RUL"] = (X.index / 256).astype("int")[::-1].values
+            X["RUL"] = (X.index / 2560).astype("int")[::-1].values
             # TODO: Truncate?
             # if RULS is not None:
             #     X["RUL"] += RULS[bearing.split("/")[-1]]
